18af960279ff91cc1d6c6e1d34b2564a526ef29f
xuyc <3099817419@qq.com>
2021-11-28 22:31:35 +0800

    学习笔记

--- src/components/User.vue
+++ src/components/User.vue
@@ -2,6 +2,7 @@
   <div>
     <h2>xxxxx</h2>
     <h2>{{userId}}</h2>
+    <button @click="btnClick">查询</button>
   </div>
 </template>
 
@@ -17,6 +18,14 @@ export default {
     userId (){
       //$route代表当前活跃的url。
       return this.$route.params.userId
+    }
+  },
+  methods:{
+    btnClick(){
+      //所有的组件都继承自Vue类的原型
+      console.log(this.$router);
+      console.log(this.$route);
+      this.test();
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import router from "./router";
 
 Vue.config.productionTip = false
+//向Vue原型上添加属性，可全局使用。
+Vue.prototype.test = function (){
+  console.log('test');
+}
 
 new Vue({
   render: h => h(App),
   router
 }).$mount('#app')
+
+console.log(router);
