3fae949063a5e600686220e67f2cbf091c5c5b45
xuy <3099817419@qq.com>
2021-12-02 17:35:59 +0800

    tabbar组件抽取，125

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,16 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    <tar-bar>
-      <tab-bar-item path="/home" activeColor="#669600">
-        <template v-slot:item-icon>
-          <img src="./assets/img/tabbar/icon01.png" alt="">
-        </template>
-        <template v-slot:item-icon-active>
-          <img src="./assets/img/tabbar/icon01_active.png" alt="">
-        </template>
-        <template v-slot:item-text>
-          <div >首页</div>
-        </template>
-      </tab-bar-item>
-      <tab-bar-item path="/category" activeColor="#886960">
-        <template v-slot:item-icon>
-          <img src="./assets/img/tabbar/icon02.png" alt="">
-        </template>
-        <template v-slot:item-icon-active>
-          <img src="./assets/img/tabbar/icon02_active.png" alt="">
-        </template>
-        <template v-slot:item-text>
-          <div >分类</div>
-        </template>
-      </tab-bar-item>
-      <tab-bar-item path="/cart" activeColor="#119600">
-        <template v-slot:item-icon>
-          <img src="./assets/img/tabbar/icon03.png" alt="">
-        </template>
-        <template v-slot:item-icon-active>
-          <img src="./assets/img/tabbar/icon03_active.png" alt="">
-        </template>
-        <template v-slot:item-text>
-          <div >购物车</div>
-        </template>
-      </tab-bar-item>
-      <tab-bar-item path="/profile">
-        <template v-slot:item-icon>
-          <img src="./assets/img/tabbar/icon04.png" alt="">
-        </template>
-        <template v-slot:item-icon-active>
-          <img src="./assets/img/tabbar/icon04_active.png" alt="">
-        </template>
-        <template v-slot:item-text>
-          <div >个人</div>
-        </template>
-      </tab-bar-item>
-    </tar-bar>
+    <main-tabbar></main-tabbar>
   </div>
 </template>
 
 <script>
-import TarBar from "./components/TabBar/TarBar";
-import TabBarItem from "./components/TabBar/TabBarItem";
+import MainTabbar from "./views/mainTabbar/MainTabbar";
 export default {
   name: 'App',
   components: {
-    TarBar,
-    TabBarItem
+    MainTabbar
   },
   data() {
     return {
---
+++ src/views/mainTabbar/MainTabbar.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <tar-bar>
+      <tab-bar-item path="/home" activeColor="#669600">
+        <template v-slot:item-icon>
+          <img src="../../assets/img/tabbar/icon01.png" alt="">
+        </template>
+        <template v-slot:item-icon-active>
+          <img src="../../assets/img/tabbar/icon01_active.png" alt="">
+        </template>
+        <template v-slot:item-text>
+          <div >首页</div>
+        </template>
+      </tab-bar-item>
+      <tab-bar-item path="/category" activeColor="#886960">
+        <template v-slot:item-icon>
+          <img src="../../assets/img/tabbar/icon02.png" alt="">
+        </template>
+        <template v-slot:item-icon-active>
+          <img src="../../assets/img/tabbar/icon02_active.png" alt="">
+        </template>
+        <template v-slot:item-text>
+          <div >分类</div>
+        </template>
+      </tab-bar-item>
+      <tab-bar-item path="/cart" activeColor="#119600">
+        <template v-slot:item-icon>
+          <img src="../../assets/img/tabbar/icon03.png" alt="">
+        </template>
+        <template v-slot:item-icon-active>
+          <img src="../../assets/img/tabbar/icon03_active.png" alt="">
+        </template>
+        <template v-slot:item-text>
+          <div >购物车</div>
+        </template>
+      </tab-bar-item>
+      <tab-bar-item path="/profile">
+        <template v-slot:item-icon>
+          <img src="../../assets/img/tabbar/icon04.png" alt="">
+        </template>
+        <template v-slot:item-icon-active>
+          <img src="../../assets/img/tabbar/icon04_active.png" alt="">
+        </template>
+        <template v-slot:item-text>
+          <div >个人</div>
+        </template>
+      </tab-bar-item>
+    </tar-bar>
+  </div>
+</template>
+
+<script>
+import TarBar from "../../components/TabBar/TarBar";
+import TabBarItem from "../../components/TabBar/TabBarItem";
+
+export default {
+  name: "MainTabbar",
+  components: {
+    TarBar,
+    TabBarItem
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
