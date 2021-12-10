813a7e114eb87338aa839b6189fa99e32e07cf51
xuy <3099817419@qq.com>
2021-12-10 14:16:11 +0800

    sass

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,6 +4,9 @@
     <promise1></promise1>
     <vuex v-show="false"></vuex>
     <axioslearn></axioslearn>
+    <div class="red">
+      sass学习
+    </div>
   </div>
 
 </template>
@@ -13,12 +16,24 @@
 import Promise1 from "../promiseLearning/promise1";
 import Vuex from "../vuexLearning/vuex";
 import Axioslearn from "../axiosLearning/Axioslearn";
+
 export default {
   name: "Home",
   components: {Axioslearn, Vuex, Promise1},
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  $color:red;
+  .red{
+    color:$color;
+    text-align: center;
+    margin-top: 50px;
+    border{
+      color: aqua;
+      style:solid;
+      width: 2px;
+    };
+  }
 
 </style>
